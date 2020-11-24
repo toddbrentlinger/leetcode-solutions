8415f760b68ccf09b20b805be830435f32608805
Todd Brentlinger <toddbrentlinger@gmail.com>
2020-11-23 20:15:08 -0800

    Added binary search solution to Search Insert Position problem

--- problems/easy/searchInsertPosition.js
+++ problems/easy/searchInsertPosition.js
@@ -7,6 +7,29 @@
  * @return {number}
  */
 var searchInsert = function (nums, target) {
+    let mid,
+        start = 0,
+        end = nums.length - 1;
+
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
+        if (target > nums[mid])
+            start = mid + 1;
+        else if (target < nums[mid])
+            end = mid - 1;
+        else
+            return mid;
+    }
+    return start;
+};
+
+/**
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert01 = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] >= target)
             return i;
