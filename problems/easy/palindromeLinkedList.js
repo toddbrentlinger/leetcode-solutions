971d2b8483a30324e7fc8c99a4993bb9b4ac02a3
toddbrentlinger <toddbrentlinger@gmail.com>
2022-04-02 20:12:04 -0700

    Add improved palindrome linked list solution

--- problems/easy/palindromeLinkedList.js
+++ problems/easy/palindromeLinkedList.js
@@ -1,5 +1,8 @@
 "use strict";
 
+//import { createLinkedListFromArray, convertLinkedListToArray } from '/data-structures/linkedList.js';
+import * as LinkedList from '/data-structures/linkedList.js';
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -12,6 +15,48 @@
  * @return {boolean}
  */
 var isPalindrome = function (head) {
+    // Base case: Return true if single length list
+    if (head.next === null)
+        return true;
+
+    // Find middle of linked list using tortoise-hare algorithm with slow and fast pointers
+    let slowNode = head;
+    let fastNode = head;
+    while (fastNode !== null && fastNode.next !== null) {
+        slowNode = slowNode.next;
+        fastNode = fastNode.next.next;
+    }
+
+    // Reverse second half of linked list
+    // Set middle node next property to null
+    // Every node after, set next property to previous node
+    // NOTE: Use fastNode to hold temporary reference to next node
+    let prevNode = slowNode;
+    slowNode = slowNode.next;
+    prevNode.next = null;
+    while (slowNode !== null) {
+        fastNode = slowNode.next;
+        slowNode.next = prevNode;
+        prevNode = slowNode;
+        slowNode = fastNode;
+    }
+    // prevNode points to last element of original linked list
+    // but is now reversed and ends with original middle node
+
+    // By setting fastNode to start of list, can now compare each node from 
+    // start/end toward the middle of the list. If any two values are not equal,
+    // list is NOT a palindrome.
+    fastNode = head;
+    while (prevNode !== null) {
+        if (prevNode.val !== fastNode.val)
+            return false;
+        prevNode = prevNode.next;
+        fastNode = fastNode.next;
+    }
+    return true;
+};
+
+var isPalindrome01 = function (head) {
     // Use tortoise-hare algorithm with slowPtr and fastPtr to find middle of list
     // Save each value in first half of list into array
     // Compare second half of list to array of values in reverse order
@@ -52,3 +97,31 @@ var isPalindrome = function (head) {
     // If reach here, is palindrome
     return true;
 };
+
+function isPalindromeUnitTestSingle(head, expectedOutput) {
+    console.assert(
+        isPalindrome(head) === expectedOutput,
+        {
+            llist: LinkedList.convertLinkedListToArray(head),
+            expectedOutput: expectedOutput,
+        }
+    );
+}
+
+function isPalindromeUnitTests() {
+    let llist;
+
+    llist = LinkedList.createLinkedListFromArray([1]);
+    isPalindromeUnitTestSingle(llist, true);
+
+    llist = LinkedList.createLinkedListFromArray([1, 2]);
+    isPalindromeUnitTestSingle(llist, false);
+
+    llist = LinkedList.createLinkedListFromArray([1, 2, 1]);
+    isPalindromeUnitTestSingle(llist, true);
+
+    llist = LinkedList.createLinkedListFromArray([1, 2, 2, 1]);
+    isPalindromeUnitTestSingle(llist, true);
+}
+
+isPalindromeUnitTests();
