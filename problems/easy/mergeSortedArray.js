08472b74ef59a73d5fdb796dde2e750e2c1ba468
toddbrentlinger <toddbrentlinger@gmail.com>
2022-11-10 14:31:53 -0800

    Fix error in Merge Sorted Array easy problem
    Adds function to create random test with problem constraints.
    Fixes algorithm to pass example tests and randomly computed tasks.

--- problems/easy/mergeSortedArray.js
+++ problems/easy/mergeSortedArray.js
@@ -12,13 +12,12 @@
     while (j >= 0) {
         if (i < 0 || nums1[i] < nums2[j]) {
             nums1[i+j+1] = nums2[j];
-        } else { // Else nums1[i] >= nums2[j]
+            j--;
+        } else { // Else (i >=0 && nums1[i] >= nums2[j])
             nums1[i+j+1] = nums1[i];
             nums1[i+j] = nums2[j];
             i--;
         }
-
-        j--;
     }
 };
 
@@ -45,7 +44,7 @@ var mergeUnitTest = function(input, expectedOutput) {
 };
 
 /** */
-var mergeUnitTestAll = function() {
+var mergeUnitTestAll = function(nRandomTests = 0) {
     const tests = [
         {
             input: {
@@ -92,16 +91,38 @@ var mergeUnitTestAll = function() {
             },
             expectedOutput: [1,2,3,4],
         },
+        {
+            input: {
+                nums1: [4,5,6,0,0,0],
+                m: 3,
+                nums2: [1,2,3],
+                n: 3
+            },
+            expectedOutput: [1,2,3,4,5,6],
+        },
     ];
 
     let bDoAllTestsPass = true;
     let bTestSuccess;
+    let randomTest;
+
+    // Example tests
     tests.forEach((test) => {
         bTestSuccess = mergeUnitTest(test.input, test.expectedOutput);
         if (!bTestSuccess && bDoAllTestsPass) {
             bDoAllTestsPass = false;
         }
     });
+
+    // Random tests
+    for (let i = nRandomTests; i > 0; i--) {
+        randomTest = createRandomTest(10,10);
+        bTestSuccess = mergeUnitTest(randomTest.input, randomTest.expectedOutput);
+        if (!bTestSuccess && bDoAllTestsPass) {
+            bDoAllTestsPass = false;
+        }
+    }
+
     if (bDoAllTestsPass) {
         console.log('All Tests Pass!');
     }
@@ -111,7 +132,7 @@ var mergeUnitTestAll = function() {
  * 
  * @param {Number[]} arr1 
  * @param {Number[]} arr2
- * @return {Boolean} 
+ * @return {Boolean}
  */
 var areArraysEqual = function(arr1, arr2) {
     // Compare lengths
@@ -127,6 +148,116 @@ var areArraysEqual = function(arr1, arr2) {
 };
 
 /**
+ * 
+ * @param {Number} valLimit Highest positive and lowest negative value range
+ * @param {Number} maxLength Max length of array
+ * @returns {Object}
+ */
+var createRandomTest = function(valLimit = Math.pow(10, 9), maxLength = 200) {
+    // Random m and n values
+    m = Math.floor(Math.random() * (maxLength + 1)); // 0-maxLength
+    nMax = maxLength - m + 1;
+    n = Math.floor(Math.random() * nMax);
+    if (m == 0 && n == 0) {
+        m = 1;
+    }
+
+    // Create arrays initialized with all zeros
+    let nums1 = [], nums2 = [];
+    nums1.length = m;
+    nums2.length = n;
+
+    // Populate arrays with random values
+    for (let i = 0; i < m; i++) {
+        nums1[i] = Math.floor(Math.random() * valLimit * 2) - valLimit;
+    }
+    for (let i = 0; i < n; i++) {
+        nums2[i] = Math.floor(Math.random() * valLimit * 2) - valLimit;
+    }
+    
+    // Initialize expected output array by combining nums1 and nums2
+    let expectedOutput = nums1.concat(nums2);
+
+    // Sort arrays
+    var sortByValue = function (a,b) {
+        return a - b;
+    };
+    nums1.sort(sortByValue);
+    nums2.sort(sortByValue);
+    expectedOutput.sort(sortByValue);
+
+    // Append n zeros to end of nums1
+    nums1 = nums1.concat(new Array(n).fill(0));
+
+    return {
+        input: {
+            nums1,
+            m,
+            nums2,
+            n,
+        },
+        expectedOutput,
+    };
+};
+
+/**
+
+New Algorithm
+ 
+nums1: [4,5,6,0,0,0],
+m: 3,
+nums2: [1,2,3],
+n: 3
+nums1(final): [1,2,3,4,5,6]
+
+i = m-1 = 2, j = n-1 = 2, nums1[i] = 6, nums2[j] = 3
+nums1[i] > nums2[j]
+nums1[i+j+1] = nums1[i] => nums1[5] = 6
+nums1[i+j] = nums2[j] => nums1[4] = 3
+decrement i
+nums1 = [4,5,6,0,3,6]
+
+i = 1, j = 1, nums1[i] = 5, nums2[j] = 3
+nums1[i] > nums2[j]
+nums1[i+j+1] = nums1[i] => nums1[4] = 5
+nums1[i+j] = nums2[j] => nums1[3] = 3
+decrement i
+nums1 = [4,5,6,3,5,6]
+
+i = 0, j = 2, nums1[i] = 4, nums2[j] = 3
+nums1[i] > nums2[j]
+nums1[i+j+1] = nums1[i] => nums1[3] = 4
+nums1[i+j] = nums2[j] => nums1[2] = 3
+decrement i
+nums1 = [4,5,3,4,5,6]
+
+i = -1, j = 2, nums1[i] = error, nums2[j] = 3
+i < 0
+nums1[i+j+1] = nums2[j] => nums1[2] = 3
+decrement j
+nums1 = [4,5,3,4,5,6]
+
+i = -1, j = 1, nums1[i] = error, nums2[j] = 2
+i < 0
+nums1[i+j+1] = nums2[j] => nums1[1] = 2
+decrement j
+nums1 = [4,2,3,4,5,6]
+
+i = -1, j = 0, nums1[i] = error, nums2[j] = 1
+i < 0
+nums1[i+j+1] = nums2[j] => nums1[0] = 1
+decrement j
+nums1 = [1,2,3,4,5,6]
+
+i = -1, j = -1
+j < 0
+end loop
+nums1(final) = [1,2,3,4,5,6]
+
+*******************************************************************************
+
+Old Algorithm
+
 nums1: [1,2,3,0,0,0],
 m: 3,
 nums2: [2,5,6],
@@ -148,7 +279,7 @@ nums1 = [1,2,3,0,5,6]
 i = 2, j = 0, nums1[i] = 3, nums2[j] = 2
 nums1[i] > nums2[j]
 nums1[i+j+1] = nums1[i] => nums1[3] = 3
-nums1[i+j] = nums2[j] => 
+nums1[i+j] = nums2[j] => nums1[2] = 2
 decrement i & j
 nums1 = [1,2,2,3,5,6]
 
