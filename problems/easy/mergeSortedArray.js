38d7d5c8abf2f5aecc860e5162d4f5f901ac45f9
toddbrentlinger <toddbrentlinger@gmail.com>
2022-11-11 13:01:07 -0800

    Improve solution for Merge Sorted Array easy problem
    Uses arguments m and n as index counters instead of adding two new
    variables. This improves memory and runtime distribution.

--- problems/easy/mergeSortedArray.js
+++ problems/easy/mergeSortedArray.js
@@ -6,6 +6,29 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
  var merge = function(nums1, m, nums2, n) {
+    m--;
+    n--;
+
+    while (n >= 0) {
+        if (m < 0 || nums1[m] < nums2[n]) {
+            nums1[m+n+1] = nums2[n];
+            n--;
+        } else { // Else (m >=0 && nums1[m] >= nums2[n])
+            nums1[m+n+1] = nums1[m];
+            nums1[m+n] = nums2[n];
+            m--;
+        }
+    }
+};
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+ var mergeOld = function(nums1, m, nums2, n) {
     let i = m - 1;
     let j = n - 1;
 
