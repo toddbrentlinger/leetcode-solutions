57480b868db6ae92f0e50d6050a9d08ecff1daa3
toddbrentlinger <toddbrentlinger@gmail.com>
2023-08-22 15:20:04 -0700

    Add improved solution for Pascal's triangle
    Adds different solutions with increasingly lower runtimes ending
    with solution that beats 98% of other JavaScript solutions.

--- problems/easy/pascalsTriangle.js
+++ problems/easy/pascalsTriangle.js
@@ -3,6 +3,108 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
+    let output = [[1]];
+    let currRow = 1;
+    let i;
+
+    while (currRow < numRows) {
+        output[currRow] = [...output[currRow - 1], 1];
+
+        for (i = 1; i < currRow; i++) {
+            output[currRow][i] = output[currRow - 1][i - 1] + output[currRow - 1][i];
+        }
+
+        currRow++;
+    }
+
+    return output;
+};
+
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate4 = function(numRows) {
+    let output = [[1]];
+    let prevRow = 0;
+    let i;
+
+    while (prevRow < numRows - 1) {
+        output[prevRow + 1] = [...output[prevRow], 1];
+
+        for (i = 1; i < prevRow + 1; i++) {
+            output[prevRow + 1][i] = output[prevRow][i - 1] + output[prevRow][i];
+        }
+
+        prevRow++;
+    }
+
+    return output;
+};
+
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate3 = function(numRows) {
+    switch(numRows) {
+        case 1:
+            return [[1]];
+        case 2:
+            return [[1], [1,1]];
+        default:
+            let output = [[1], [1,1]];
+            let prevRow = 1;
+            let i;
+            
+            while (prevRow < numRows - 1) {
+                output[prevRow + 1] = [...output[prevRow], 1];
+
+                for (i = 1; i < prevRow + 1; i++) {
+                    output[prevRow + 1][i] = output[prevRow][i - 1] + output[prevRow][i];
+                }
+
+                prevRow++;
+            }
+
+            return output;
+    }
+};
+
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate2 = function(numRows) {
+    switch(numRows) {
+        case 1:
+            return [[1]];
+        case 2:
+            return [[1], [1,1]];
+        default:
+            let output = [[1], [1,1]];
+            let prevRow = 1;
+            let i;
+            
+            while (prevRow < numRows - 1) {
+                output[prevRow + 1] = Array(prevRow + 2).fill(1);
+
+                for (i = 1; i < prevRow + 1; i++) {
+                    output[prevRow + 1][i] = output[prevRow][i - 1] + output[prevRow][i];
+                }
+
+                prevRow++;
+            }
+
+            return output;
+    }
+};
+
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate1 = function(numRows) {
     switch(numRows) {
         case 1:
             return [[1]];
