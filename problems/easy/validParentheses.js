d45bcf31b3df9c7ef830cc4cf146bbb54a3886b8
toddbrentlinger <toddbrentlinger@gmail.com>
2023-09-01 18:20:22 -0700

    Add improved solution for valid parentheses problem
    Reverses for-loop so conditional compares to zero
    instead of calculating array length every time.
    Calls map key and compares to undefined if NOT present instead
    of using hasOwnProperty method.

--- problems/easy/validParentheses.js
+++ problems/easy/validParentheses.js
@@ -1,6 +1,33 @@
 "use strict";
 
 /**
+ * @param {string} s
+ * @return {boolean}
+ * Runtime: 48ms (93.34%)
+ * Memory: 42.00MB (85.31%)
+ */
+var isValid = function(s) {
+    let stack = [];
+    const parenthesesMap = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+    
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (parenthesesMap[s[i]]) {
+            stack.push(parenthesesMap[s[i]]);
+        } else {
+            if (stack.pop() !== s[i]) {
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
+};
+
+/**
  * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
  * An input string is valid if:
  * 1) Open brackets must be closed by the same type of brackets.
