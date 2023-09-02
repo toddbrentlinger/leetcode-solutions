59870a0720a833d0d53c9c091310f2c5047d8c2d
toddbrentlinger <toddbrentlinger@gmail.com>
2023-09-01 18:17:08 -0700

    Add improved solution for Is Palindrome problem
    Reverses for-loop so conditional tested on every loop compares to zero
    instead of calculating array length every time.

--- problems/easy/isPalindrome.js
+++ problems/easy/isPalindrome.js
@@ -1,6 +1,27 @@
 ﻿"use strict";
 
 /**
+ * @param {number} x
+ * @return {boolean}
+ * Runtime: 129ms (90.72%)
+ * Memory: 51.65MB (33.78%)
+ */
+var isPalindrome = function(x) {
+    // Check negative numbers (NOT palindrome)
+    if (x < 0)
+        return false;
+    
+    const numStr = x.toString();
+
+    for (let i = Math.floor(numStr.length / 2) - 1; i >= 0; i--) {
+        if (numStr[i] !== numStr[numStr.length - i - 1])
+            return false;
+    }
+
+    return true;
+};
+
+/**
  * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
  * @param {number} x
  * @return {boolean}
