a4e50d2df14b3e91d78c87e2d1d8d95270574960
toddbrentlinger <toddbrentlinger@gmail.com>
2023-09-23 18:54:16 -0700

    Add most liked solution to intersection of two linked lists

--- problems/easy/intersectionOfTwoLinkedLists.js
+++ problems/easy/intersectionOfTwoLinkedLists.js
@@ -10,10 +10,38 @@
  * @param {ListNode} headA
  * @param {ListNode} headB
  * @return {ListNode}
+ * Non-Custom
+ * Runtime: 66ms (94.33%)
+ * Memory: 49.90MB (54.07%)
+ */
+var getIntersectionNode = function(headA, headB) {
+    let a = headA, b = headB;
+
+    while (a !== b) {
+        a = (a === null) ? headB : a.next;
+        b = (b === null) ? headA : b.next;
+    }
+
+    return a;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ * Custom
  * Runtime: 71ms (84.51%)
  * Memory: 51.22MB (11.65%)
  */
-var getIntersectionNode = function(headA, headB) {
+var getIntersectionNode1 = function(headA, headB) {
     let set = new Set();
 
     let node = headA;
