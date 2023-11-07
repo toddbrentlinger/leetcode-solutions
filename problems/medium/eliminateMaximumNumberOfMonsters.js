98737f469fd0b29e1a9c3f42c0aa82bb1c47e85c
toddbrentlinger <toddbrentlinger@gmail.com>
2023-11-07 15:27:45 -0800

    Add solution for medium daily problem 11/7/23
    Adds solution for eliminate maximum number of monsters medium daily
    problem using O(nlogn) time complexity and O(n) space complexity.

---
+++ problems/medium/eliminateMaximumNumberOfMonsters.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[]} dist
+ * @param {number[]} speed
+ * @return {number}
+ * Runtime: 139ms (48.65%)
+ * Memory: 55.2MB (72.97%)
+ * Time Complexity: O(nlogn)
+ * Space Complexity: O(n)
+ */
+var eliminateMaximum = function(dist, speed) {
+    // Array of turns each monster needs to reach Player, sorted in ascending order
+    let turns = dist.map((distVal, i) => {
+        return Math.ceil(distVal / speed[i]);
+    }).sort((a,b) => a - b);
+    
+    // If any monster's turns is less than or equal to it's index,
+    // Player will not be able to eliminate it on that turn
+    let i;
+    for (i = 0; i < turns.length; i++) {
+        if (turns[i] <= i) {
+            return i;
+        }
+    }
+
+    // If reach here, all monsters have been eliminated
+    return i;
+};
+
+/**
+dist = [3,2,4], speed = [5,3,2]
+
+Must predict next round of dist by dist[i] - speed[i]
+dist2 = [-2,-1,2]
+i=0 will reach first since smallest value (hit)
+i=1 is also <= 0 so this monster will make us lose
+
+Create new array, turns[], to hold number of turns before reaching player, then sort in ascending order.
+Find first i where:
+1) turns[i] <= i meaning monster will attack before attack turn can be made
+
+dist=[1,1,2,3]
+speed=[1,1,1,1]
+
+turns=[1,1,2,3]
+i=0 val=1 > i hit
+i=1 val=1 <= i miss return i
+
+ */
