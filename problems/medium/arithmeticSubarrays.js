65fa060b4b8fa9be122ee9bc6865ff940b5a8aff
toddbrentlinger <toddbrentlinger@gmail.com>
2023-11-25 11:55:17 -0800

    Add improved solution for arithmetic subarrays problem
    Adds solution for arithmetic subarrays medium problem that uses NO
    sorting.

--- problems/medium/arithmeticSubarrays.js
+++ problems/medium/arithmeticSubarrays.js
@@ -1,3 +1,67 @@
+// ----------------------------------------------------------------------------
+// No Sorting
+// ----------------------------------------------------------------------------
+
+/**
+ * @param {number[]} nums
+ * @param {number[]} l
+ * @param {number[]} r
+ * @return {boolean[]}
+ * Runtime: 92ms (85.89%)
+ * Memory: 49.3MB (12.9%)
+ * Time Complexity: O(n) where n is nums.length if entire nums is tested as one subarray
+ * Space Complexity: O(n) where n is nums.length if entire nums is tested as one subarray
+ */
+var checkArithmeticSubarrays = function(nums, l, r) {
+    let min, max, diff, val;
+    let valSet = new Set();
+
+    return l.map((start, i) => {
+        // Clear value set from previous subarray
+        valSet.clear();
+        
+        // Find min/max and create set of values in subarray
+        min = nums[start];
+        max = min;
+        valSet.add(min);
+        for (start++; start <= r[i]; start++) {
+            if (nums[start] < min) { min = nums[start]; }
+            else if (nums[start] > max) { max = nums[start]; }
+            valSet.add(nums[start]);
+        }
+
+        // If min/max are same, subarray are all same value and is arithmetic subarray
+        if (min === max) { return true; }
+
+        // Find diff required for arithmetic subarray using min/max and length of subarray
+        diff = (max - min) / (r[i] - l[i]);
+
+        // If diff is NOT an integer, subarray cannot be an arithmetic subarray since values are only integers
+
+        // Check if every value that should be in arithmetic subarray is in set
+        val = min + diff;
+        while (val < max) {
+            // If value is NOT in set, subarray cannot be arithmetic subarray
+            if (!valSet.has(val)) {
+                return false;
+            }
+            // Set next value that needs to be in arithmetic subarray
+            val += diff;
+        }
+
+        return true;
+    });
+};
+
+/**
+- If 0 -> 2 is false, then 0 -> 2+ will be false as well, so can keep a map of start index AND end index
+for know false subarrays. Value of each key can be increased if 
+ */
+
+// ----------------------------------------------------------------------------
+// Sorting
+// ----------------------------------------------------------------------------
+
 /**
  * @param {number[]} nums
  * @param {number[]} l
