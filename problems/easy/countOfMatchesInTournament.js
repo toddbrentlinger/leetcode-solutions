d6e894dff6669382d72b9337b1f4b55c94c347e7
toddbrentlinger <toddbrentlinger@gmail.com>
2023-12-04 18:49:22 -0800

    Add improved solution for easy daily problem.
    Adds improved solution for count of matches in tournament easy daily
    problem with time and space complexity of O(1).

--- problems/easy/countOfMatchesInTournament.js
+++ problems/easy/countOfMatchesInTournament.js
@@ -1,8 +1,30 @@
 /**
  * @param {number} n
  * @return {number}
- * Runtime: ms (%)
- * Memory: MB (%)
+ * Runtime: 45ms (89.43%)
+ * Memory: 42.13MB (15.41%)
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+var numberOfMatches = function(n) {
+    return n - 1;
+};
+
+/**
+Number of matches will always be one less than number of teams.
+1 - 0
+2 - 1
+3 - 2
+4 - 3
+5 - 4
+6 - 5
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ * Runtime: 49ms (71.30%)
+ * Memory: 42.2MB (15.41%)
  * Time Complexity: O(logn) n is approximately halved every loop
  * Space Complexity: O(1)
  */
