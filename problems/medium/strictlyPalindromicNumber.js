ee2760e981b4dc90b4444e7edffd6c7e35bb3d31
toddbrentlinger <toddbrentlinger@gmail.com>
2023-12-16 11:28:00 -0800

    Add solution to medium problem 2396
    Adds solution to strictly palindromic number medium problem with time and
    space complexity of O(1).

---
+++ problems/medium/strictlyPalindromicNumber.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ * Runtime: 43ms (92.16%)
+ * Memory: 42.01MB (18.63%)
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+var isStrictlyPalindromic = function(n) {
+    // Never possible
+    return false;
+};
+
+/**
+9
+2: 1001 => 1+2^3
+3:  100 => 3^2
+4:  NOT POSSIBLE
+5:  NOT POSSIBLE
+...
+
+ */
